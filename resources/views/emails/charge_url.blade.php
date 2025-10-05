165cc383ca283443504e75ab902fd02a4095eed2
unknown <yumakparobo@gmail.com>
2025-10-05 19:55:40 +0100

    Created chargeUrl trans email view

---
+++ resources/views/emails/charge_url.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/emails/charge_url.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Charge URL</title>
+</head>
+<body>
+    <p>Dear Customer,</p>
+    <p>Your charge URL is: <a href="{{ $charge_url2 }}">{{ $charge_url2 }}</a></p>
+    <p>Thank you for your transaction.</p>
+</body>
+</html>
