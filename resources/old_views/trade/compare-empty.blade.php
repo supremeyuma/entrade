e5424dca17d4a4fe86332323115519fd90547571
unknown <yumakparobo@gmail.com>
2026-02-19 21:42:16 +0100

    fixed guest layout referenced in app/GuestLayout.php

--- resources/old_views/trade/compare-empty.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container text-center">
-    <h2>No traders selected for comparison</h2>
-    <p>Please add traders to compare from the leaderboard or their profile page.</p>
-    <a href="{{ route('leaderboard') }}" class="btn btn-primary">Go to Leaderboard</a>
-</div>
-@endsection
