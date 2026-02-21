de998f42c1a197fdd0d16050acfd0056cb13b8a2
unknown <yumakparobo@gmail.com>
2026-02-21 19:35:08 +0100

    fixed the charge_url email

--- resources/views/emails/charge_url.blade.php
+++ resources/views/emails/charge_url.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body>
     <p>Dear Customer,</p>
-    <p>Your charge URL is: <a href="{{ $charge_url2 }}">{{ $charge_url2 }}</a></p>
+    <p>Your charge URL is: <a href="{{ $charge_url }}">{{ $charge_url }}</a></p>
     <p>Thank you for your transaction.</p>
 </body>
 </html>
