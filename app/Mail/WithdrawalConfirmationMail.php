af1972b40d4a6dd346379db2d6262fe74d7097d6
CodexSandboxOffline <yumakparobo@gmail.com>
2026-03-28 03:31:02 +0100

    Fix withdrawal confirmation mail payload

--- app/Mail/WithdrawalConfirmationMail.php
+++ app/Mail/WithdrawalConfirmationMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\Withdrawal;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -13,12 +14,14 @@ class WithdrawalConfirmationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public Withdrawal $withdrawal;
+
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(Withdrawal $withdrawal)
     {
-        //
+        $this->withdrawal = $withdrawal;
     }
 
     /**
